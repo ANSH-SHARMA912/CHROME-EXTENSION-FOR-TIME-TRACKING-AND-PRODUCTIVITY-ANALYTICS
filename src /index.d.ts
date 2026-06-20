b3568b15dcd48242d14db9950345d0840699e5b1
ANSH-SHARMA912 <166865958+ANSH-SHARMA912@users.noreply.github.com>
2026-06-20 16:49:19 +0530

    Add TypeScript definitions for origins and customScripts

---
+++ src /index.d.ts
@@ -0,0 +1,19 @@
+import origins, { CustomScripts as customScripts } from './origins';
+
+declare const _default = origins as {
+  [key: string]: {
+    name: string;
+    url: string;
+    file?: string;
+    aliasOf?: string;
+  };
+};
+
+export default _default;
+
+export declare const CustomScripts = customScripts as {
+  [key: string]: {
+    name: string;
+    file?: string;
+  };
+}
