cf88e2ce8f7bee5221bba5c8b032897dea0099e5
ANSH-SHARMA912 <166865958+ANSH-SHARMA912@users.noreply.github.com>
2026-06-20 16:49:55 +0530

    Add files via upload

---
+++ src/custom-scripts.js
@@ -0,0 +1,14 @@
+export const CustomScripts = {
+  'script_a_shortcode': {
+    name: 'Custom Script For XYZ',
+    file: '_custom_SampleScript.js',
+  },
+  'nfJiraScript': {
+    name: 'Custom Jira Script',
+    file: '_custom_nf_jira.js',
+  },
+  'nfGitlabScript': {
+    name: 'Custom Gitlab Script',
+    file: '_custom_nf_gitlab.js',
+  },
+}
